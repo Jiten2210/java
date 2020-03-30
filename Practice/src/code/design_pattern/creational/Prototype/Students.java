1fed4ce93fdf1c076be8781e19e2e2310f5c561c
Jitu <jitendrabisht2210@gmail.com>
2020-03-30 09:39:31 +0530

    Creational Pattern - Prototype Pattern

--- Practice/src/code/design_pattern/creational/Prototype/Students.java
+++ Practice/src/code/design_pattern/creational/Prototype/Students.java
@@ -1,4 +1,41 @@
 package code.design_pattern.creational.Prototype;
 
-public class Employees {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Students implements Cloneable {
+
+    public List<String> studentsList;
+
+    public Students() {
+        studentsList = new ArrayList<String>();
+    }
+
+
+    public Students(List<String> studentsList) {
+        this.studentsList = studentsList;
+    }
+
+    public void addStudents() {
+        //read students data from database and put into list
+        studentsList.add("Jitu");
+        studentsList.add("Tanu");
+        studentsList.add("Pooja");
+        studentsList.add("Sonu");
+        studentsList.add("Krishty");
+    }
+
+    public List<String> getStudentsList() {
+        return studentsList;
+    }
+
+    //to provide a deep copy of the students list.
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        List<String> studentsListTemp = new ArrayList<String>();
+        for (String s : this.getStudentsList()) {
+            studentsListTemp.add(s);
+        }
+        return new Students(studentsListTemp);
+    }
 }
