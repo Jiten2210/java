f50771c3810b69fc0f0e0df0a610e76776c40298
Jitu <jitendrabisht2210@gmail.com>
2020-04-14 22:24:37 +0530

    Leetcode problems

---
+++ Practice/src/code/leet_code/easy/RemoveDuplicatesII.java
@@ -0,0 +1,21 @@
+package code.leet_code.easy;
+
+public class RemoveDuplicatesII {
+    public static void main(String[] args) {
+        Integer a[] = {0,0,1,1,1,1,2,3,3};
+        System.out.println(removeDuplicateElements(a));
+    }
+
+    private static int removeDuplicateElements(Integer[] nums) {
+
+        if (nums.length == 0) return 0;
+
+        int i = 1;
+        for (int j = 0; j < nums.length -2; j++) {
+            if (nums[j] != nums[j+2]) {
+                nums[i++] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
