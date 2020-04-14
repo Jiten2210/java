90f9d87c096a445cb3f4689588452e9d5d787afd
Jitu <jitendrabisht2210@gmail.com>
2020-04-14 22:24:37 +0530

    Leetcode problems

---
+++ Practice/src/code/concurrency/SynchronizedMethodExample.java
@@ -0,0 +1,36 @@
+package code.concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class SynchronizedCounter {
+    private int count = 0;
+
+    // Synchronized Method
+    public synchronized void increment() {
+        System.out.println(Thread.currentThread().getName());
+        count = count + 1;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+public class SynchronizedMethodExample {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        SynchronizedCounter synchronizedCounter = new SynchronizedCounter();
+
+        for (int i = 0; i < 1000; i++) {
+            executorService.submit(() -> synchronizedCounter.increment());
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(60, TimeUnit.SECONDS);
+
+        System.out.println("Final count is : " + synchronizedCounter.getCount());
+    }
+}
