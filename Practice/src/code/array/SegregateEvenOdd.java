ca2c6804cecd0bd651f3f2af99622f9e920e4d4d
Jitu <jitendrabisht2210@gmail.com>
2020-05-03 07:15:12 +0530

    Merge Sort

--- Practice/src/code/array/SegregateEvenOdd.java
+++ Practice/src/code/array/SegregateEvenOdd.java
@@ -1,32 +1,30 @@
 package code.array;
 
+import java.util.Arrays;
+
 public class SegregateEvenOdd {
 
     public static void main(String[] args) {
         int[] a = new int[]{100, 231, 54, 342, 12, 15, 7, 34, 23, 11, 36};
-        int i;
-        int size = a.length;
-        SegregateEvenOdd s = new SegregateEvenOdd();
-        s.segregatedArray(a, size);
-        for (i = 0; i < size; i++)
-            System.out.print(a[i] + " ");
+        segregatedArray(a, a.length);
+        System.out.println(Arrays.toString(a));
     }
 
-    void segregatedArray(int[] b, int size) {
-        int l = 0;
-        int r = size - 1;
-        while (l < r) {
-            while (b[l] % 2 == 0 && l < r)
-                l++;
+    static void segregatedArray(int[] a, int size) {
+        int left = 0;
+        int right = size - 1;
+        while (left < right) {
+            while (a[left] % 2 == 0 && left < right)
+                left++;
 
-            while (b[r] % 2 == 1 && l < r)
-                r--;
-            if (l < r) {
-                int t = b[l];
-                b[l] = b[r];
-                b[r] = t;
-                l++;
-                r--;
+            while (a[right] % 2 == 1 && left < right)
+                right--;
+            if (left < right) {
+                int t = a[left];
+                a[left] = a[right];
+                a[right] = t;
+                left++;
+                right--;
             }
         }
     }
--- Practice/src/code/array/TripletSum.java
+++ Practice/src/code/array/TripletSum.java
@@ -1,26 +1,48 @@
 package code.array;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class TripletSum {
     public static void main(String[] args) {
-        int arr[] = {1, 4, 45, 6, 10, 8};
-        Arrays.sort(arr);
-        int sum = 24;
-        for (int i = 0; i < arr.length - 3; i++) {
-            int l = i + 1;
-            int r = arr.length - 1;
-            while (l < r) {
-                if (arr[i] + arr[l] + arr[r] == sum) {
-                    System.out.println("Numbers are:" + arr[i] + " " + arr[l] + " " + arr[r]);
+        int a[] = {1, 4, 45, 6, 10, 8};
+        int n = a.length;
+        findThreeSum(a, n, 24);
+        findTripletSum(a, n, 24);
+    }
+
+    private static void findTripletSum(int[] a, int n, int sum) {
+        Arrays.sort(a);
+        for (int i = 0; i < n - 3; i++) {
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                if (a[i] + a[left] + a[right] == sum) {
+                    System.out.println("\n Numbers are: " + a[i] + " " + a[left] + " " + a[right]);
                     break;
-                } else if (arr[i] + arr[l] + arr[r] < sum)
-                    l++;
+                } else if (a[i] + a[left] + a[right] < sum)
+                    left++;
 
                 else
-                    r--;
+                    right--;
             }
 
         }
+    }
+
+    static boolean findThreeSum(int a[], int arr_size, int sum) {
+        for (int i = 0; i < arr_size - 2; i++) {
+            HashSet<Integer> set = new HashSet<Integer>();
+            int currentSum = sum - a[i];
+            for (int j = i + 1; j < arr_size; j++) {
+                if (set.contains(currentSum - a[j])) {
+                    System.out.printf("Numbers are %d, %d, %d", a[i],
+                            a[j], currentSum - a[j]);
+                    return true;
+                }
+                set.add(a[j]);
+            }
+        }
+        return false;
     }
 }
