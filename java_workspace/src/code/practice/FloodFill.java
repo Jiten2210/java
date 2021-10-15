5d71a5d1c285afdf4133ac1307e9b9313bc0e2a1
Jitu <jitendrabisht2210@gmail.com>
2021-10-15 14:07:19 +0530

    flood fill dfs solution

--- java_workspace/src/code/practice/FloodFill.java
+++ java_workspace/src/code/practice/FloodFill.java
@@ -5,39 +5,37 @@ class FloodFill {
     static int M = 8;
     static int N = 8;
 
-    static void floodFillUtil(int screen[][], int x, int y,
-                              int prevC, int newC) {
-        if (x < 0 || x >= M || y < 0 || y >= N)
-            return;
-        if (screen[x][y] != prevC)
-            return;
-
-        screen[x][y] = newC;
-
-        floodFillUtil(screen, x + 1, y, prevC, newC);
-        floodFillUtil(screen, x - 1, y, prevC, newC);
-        floodFillUtil(screen, x, y + 1, prevC, newC);
-        floodFillUtil(screen, x, y - 1, prevC, newC);
+    public static int[][] dfs(int[][] image, int sr, int sc, int newColor) {
+        int color = image[sr][sc];
+        if (color != newColor) dfs(image, sr, sc, color, newColor);
+        return image;
     }
 
-    static void floodFill(int screen[][], int x, int y, int newC) {
-        int prevC = screen[x][y];
-        if (prevC == newC) return;
-        floodFillUtil(screen, x, y, prevC, newC);
+    public static void dfs(int[][] image, int x, int y, int color, int newColor) {
+        if (x < 0 || x >= M || y < 0 || y >= N || image[x][y] != color)
+            return;
+
+        image[x][y] = newColor;
+
+        dfs(image, x + 1, y, color, newColor);
+        dfs(image, x - 1, y, color, newColor);
+        dfs(image, x, y + 1, color, newColor);
+        dfs(image, x, y - 1, color, newColor);
     }
 
     public static void main(String[] args) {
-        int screen[][] = {{1, 1, 1, 1, 1, 1, 1, 1},
-                {1, 1, 1, 1, 1, 1, 0, 0},
-                {1, 0, 0, 1, 1, 0, 1, 1},
-                {1, 2, 2, 2, 2, 0, 1, 0},
-                {1, 1, 1, 2, 2, 0, 1, 0},
-                {1, 1, 1, 2, 2, 2, 2, 0},
-                {1, 1, 1, 1, 1, 2, 1, 1},
-                {1, 1, 1, 1, 1, 2, 2, 1},
-        };
+        int screen[][] =
+                {{1, 1, 1, 1, 1, 1, 1, 1},
+                        {1, 1, 1, 1, 1, 1, 0, 0},
+                        {1, 0, 0, 1, 1, 0, 1, 1},
+                        {1, 2, 2, 2, 2, 0, 1, 0},
+                        {1, 1, 1, 2, 2, 0, 1, 0},
+                        {1, 1, 1, 2, 2, 2, 2, 0},
+                        {1, 1, 1, 1, 1, 2, 1, 1},
+                        {1, 1, 1, 1, 1, 2, 2, 1},
+                };
         int x = 4, y = 4, newC = 3;
-        floodFill(screen, x, y, newC);
+        dfs(screen, x, y, newC);
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++)
                 System.out.print(screen[i][j] + " ");
